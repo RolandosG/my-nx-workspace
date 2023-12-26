d061d5cac9f6f0e475d1bf0674ad29c61c906ac0
Rolandos Georgoulis <georgoulis.rolandos@gmail.com>
2023-12-26 15:36:29 -0500

    commiting lowerCase App to upper

--- apps/inventory-management-system/src/main.tsx
+++ apps/inventory-management-system/src/main.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
-import App from './app/app';
+import App from './app/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
