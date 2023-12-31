10ab4b74ddef12a173a56dfba0eb4e779fe96986
Rolandos Georgoulis <georgoulis.rolandos@gmail.com>
2023-12-30 20:54:49 -0500

    thats a lot of stuff

--- apps/inventory-management-system/src/config.ts
+++ apps/inventory-management-system/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    apiBaseUrl: 'http://localhost:3000',
-    apiBaseWsUrl: 'ws://localhost:3000',
+  apiBaseUrl: 'https://backend-mindworks-061124d94608.herokuapp.com',
+  apiBaseWsUrl: 'wss://backend-mindworks-061124d94608.herokuapp.com',
   };
 
   export default config;
