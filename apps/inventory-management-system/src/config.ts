9af3ee516d309a432ea292b6576acf346771bd79
Rolandos Georgoulis <georgoulis.rolandos@gmail.com>
2024-01-04 02:37:06 -0500

    fixed the extra /

--- apps/inventory-management-system/src/config.ts
+++ apps/inventory-management-system/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  apiBaseUrl: 'https://smbackend-4d26baa62dad.herokuapp.com/',
-  apiBaseWsUrl: 'wss://smbackend-4d26baa62dad.herokuapp.com/',
+  apiBaseUrl: 'https://smbackend-4d26baa62dad.herokuapp.com',
+  apiBaseWsUrl: 'wss://smbackend-4d26baa62dad.herokuapp.com',
   };
 
   export default config;
