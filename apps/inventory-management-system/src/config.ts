ee35d7344c7d70bee1d58358ea7bd98082dac725
Rolandos Georgoulis <georgoulis.rolandos@gmail.com>
2024-01-04 02:26:07 -0500

    updated config

--- apps/inventory-management-system/src/config.ts
+++ apps/inventory-management-system/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  apiBaseUrl: 'https://smbackend-88278ac29518.herokuapp.com',
-  apiBaseWsUrl: 'wss://smbackend-88278ac29518.herokuapp.com',
+  apiBaseUrl: 'https://smbackend-4d26baa62dad.herokuapp.com/',
+  apiBaseWsUrl: 'wss://smbackend-4d26baa62dad.herokuapp.com/',
   };
 
   export default config;
